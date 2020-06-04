7420310c7bbb125d6fe96aac1ff7dd22d2406ab6
mrezav <muhammadreza.dev@gmail.com>
2020-06-04 20:07:21 +0700

    03 Menampilkan data contacts

--- resources/views/livewire/contact/contact-index.blade.php
+++ resources/views/livewire/contact/contact-index.blade.php
@@ -1,7 +1,27 @@
 <div>
-    <ul>
-        @foreach ($data as $contact)
-            <li>{{ $contact->name }} - {{ $contact->phone }}</li>
-        @endforeach
-    </ul>
+    <table class="table">
+        <thead class="thead-dark">
+            <tr>
+                <th scope="col">#</th>
+                <th>Name</th>
+                <th>Phone</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            @php $no = 0 @endphp
+            @foreach ($data as $contact)
+                @php $no++ @endphp
+                <tr>
+                    <td scope="row">{{ $no }}</td>
+                    <td>{{ $contact->name }}</td>
+                    <td>{{ $contact->phone }}</td>
+                    <td>
+                        <button class="btn btn-info text-white btn-sm">Edit</button>
+                        <button class="btn btn-danger text-white btn-sm">Delete</button>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 </div>
