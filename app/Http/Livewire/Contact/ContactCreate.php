b9f124b3089938287c66f1496fa392ae4353fd29
mrezav <muhammadreza.dev@gmail.com>
2020-06-04 20:28:16 +0700

    04 Memanggil Component livewire dalam livewire

---
+++ app/Http/Livewire/Contact/ContactCreate.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Contact;
+
+use Livewire\Component;
+
+class ContactCreate extends Component
+{
+    public function render()
+    {
+        return view('livewire.contact.contact-create');
+    }
+}
---
+++ resources/views/livewire/contact/contact-create.blade.php
@@ -0,0 +1,15 @@
+<div class="mt-4">
+    <form action="">
+        <div class="form-group">
+            <div class="form-row">
+                <div class="col">
+                    <input type="text" name="name" class="form-control" placeholder="Name">
+                </div>
+                <div class="col">
+                    <input type="text" name="phone" class="form-control" placeholder="Phone">
+                </div>
+            </div>
+        </div>
+            <button class="btn btn-sm btn-primary">Submit</button>
+    </form>
+</div>
